3757789c6dd1129421bf5f3a2cdb6d0bdaf9b4ab
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-03-23 20:03:42 +0600

    grand-total

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,11 +4,30 @@ import "./Cart.css"
 const Cart = (props) => 
 {
      const { cart } = props;
+     
+     let total = 0;
+     let ship = 0;
+     
+     for(const product of cart)
+     {
+         const { price , shipping } = product;
+         total = total + price;
+         ship = ship + shipping;
+     }
+
+     const tax = parseFloat((total * 0.1).toFixed(2));
+
+     const grandTotal = total + tax + ship;
+
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summary</h2>
 
             <p>Selected Item {cart.length}</p>
+            <p>Total Price : ${total}</p>
+            <p>Total Shipig : ${ship}</p>
+            <p>Tax : {tax}</p>
+            <h5>Grand Total : {grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
